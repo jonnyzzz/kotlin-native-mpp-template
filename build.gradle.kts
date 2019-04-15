8da685bd4ab08629f24266ff01857f886215ed36
Eugene Petrenko <eugene.petrenko@gmail.com>
2019-04-15 18:00:23 +0200

    ktor example

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,22 @@ plugins {
 }
 
 repositories {
+  jcenter()
   mavenCentral()
 }
 
+val ktorVersion = "1.1.3"
 dependencies {
   implementation(kotlin("stdlib"))
+
+  implementation("io.ktor:ktor-server-netty:$ktorVersion")
+  implementation("io.ktor:ktor-html-builder:$ktorVersion")
+  implementation("io.ktor:ktor-locations:$ktorVersion")
+  implementation("io.ktor:ktor-jackson:$ktorVersion")
+  implementation("org.slf4j:slf4j-simple:1.7.26")
+
+  testImplementation("junit:junit:4.12")
+  testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
 }
 
 application {
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,47 @@
 package org.jonnyzzz.template.jvm
 
-fun main() {
-  println("Hello Kotlin/JVM!")
-}
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.DefaultHeaders
+import io.ktor.features.PartialContent
+import io.ktor.features.XForwardedHeaderSupport
+import io.ktor.html.respondHtml
+import io.ktor.response.respondText
+import io.ktor.routing.Routing
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import kotlinx.html.body
+import kotlinx.html.h1
+import kotlinx.html.head
+import kotlinx.html.title
 
+
+fun main() {
+  embeddedServer(Netty, 9232) {
+    install(DefaultHeaders)
+    install(PartialContent)
+    install(XForwardedHeaderSupport)
+
+    install(Routing)
+    routing {
+      get {
+        call.respondHtml {
+          head {
+            title = "ktor!"
+          }
+
+          body {
+            //TODO: Update the code!
+            h1 { +"TODO: UPDATE ME!" }
+          }
+        }
+      }
+
+      get("/health") {
+        call.respondText { "OK" }
+      }
+    }
+  }.start(wait = true)
+}
